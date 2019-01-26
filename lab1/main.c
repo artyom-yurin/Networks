cf65dfe1ec953a7560c2782aff8a9a49b27b656d
artyom-yurin <artem_yrin@mail.ru>
2019-01-26 19:44:06 +0300

    Added help command with list of commands

--- lab1/main.c
+++ lab1/main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <signal.h>
 #include <malloc.h>
+#include <stdlib.h>
 
 struct Node {
     int value;
@@ -131,20 +132,48 @@ int main() {
 
     if (fork() == 0) //child
     {
-        while (strcmp(buffer, "quit\n") != 0)
-        {
+        while (strcmp(buffer, "quit\n") != 0) {
             close(fds[0]);
+            printf("Enter command: ");
             fgets(buffer, 1024, stdin);
             write(fds[1], buffer, 1024);
             kill(getpid(), SIGSTOP);
         }
     } else //parent
     {
-        while (strcmp(buffer, "quit\n") != 0)
-        {
+        while (strcmp(buffer, "quit\n") != 0) {
             close(fds[1]);
             read(fds[0], buffer, 1024);
-            printf("%s", buffer);
+            char *token = strtok(buffer, " ");;
+            if (strcmp(token, "push") == 0) {
+                token = strtok(NULL, "\n");
+                int number = atoi(token);
+                printf("Push %d\n", number);
+            } else if (strcmp(token, "peek\n") == 0) {
+                printf("Peek\n");
+            } else if (strcmp(token, "pop\n") == 0) {
+                printf("pop\n");
+            } else if (strcmp(token, "empty\n") == 0) {
+                printf("empty\n");
+            } else if (strcmp(token, "display\n") == 0) {
+                printf("display\n");
+            } else if (strcmp(token, "create\n") == 0) {
+                printf("create\n");
+            } else if (strcmp(token, "stack_size\n") == 0) {
+                printf("stack_size\n");
+            } else if (strcmp(token, "help\n") == 0) {
+                printf("Commands:\n");
+                printf("create - create a stack\n");
+                printf("push *value* - push some integer value to the stack\n");
+                printf("pop - pop last element from the stack\n");
+                printf("peek - return the value of top element on the stack\n");
+                printf("empty - return 1 if the stack is empty, otherwise 0\n");
+                printf("display - print all elements from top to bottom\n");
+                printf("stack_size - print current size of the stack:\n");
+                printf("quit - quit from application\n");
+            } else if (strcmp(token, "quit\n") != 0) {
+                printf("Unknown command. Use command help for look to commands list\n");
+            }
             kill(0, SIGCONT);
         }
 
